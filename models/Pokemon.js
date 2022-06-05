74b2cefc236f496ce742b658d2748e3432614794
alexace21 <alex.ace7ov@gmail.com>
2022-06-05 14:54:09 +0300

    add extra validation

--- models/Pokemon.js
+++ models/Pokemon.js
@@ -3,18 +3,28 @@ const mongoose = require('mongoose');
 const pokemonSchema = new mongoose.Schema({
     name: {
         type: String,
-        required: true,
+        required: [true, 'Pokemon title is required!'],
+        minlength: 2,
     },
     imageUrl: String,
     description: {
         type: String,
-        required: true,
+        required: [true, 'Pokemon description is required!'],
+        maxlength: 120
     },
     power: {
         type: Number,
-        required: true,
+        required: [true, 'Pokemon power is required!'],
     }
 });
+
+pokemonSchema.path('name').validate(function() {
+    return this.name >= 2 && this.name <= 10;
+}, 'Invalid pokemon title!')
+
+pokemonSchema.path('description').validate(function() {
+    return this.description <= 10;
+}, 'Invalid pokemon description!')
 
 const Pokemon = mongoose.model('Pokemon', pokemonSchema);
 
